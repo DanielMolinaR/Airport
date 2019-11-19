768f10735f217b939a9d9f3385362013be054f8f
Daniel.Molina <dani99mr31@gmail.com>
2019-11-19 18:08:15 +0100

    Lockets

--- src/airport/Conveyor.java
+++ src/airport/Conveyor.java
@@ -6,6 +6,8 @@
 package airport;
 
 import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 /**
  *
@@ -13,14 +15,44 @@ import java.util.ArrayList;
  */
 public class Conveyor {
     
-    private ArrayList<Suitcase> suitcases = new ArrayList<Suitcase>();
-    private ArrayList<Suitcase> conveyor = new ArrayList<Suitcase>();
+    ArrayList<Suitcase> conveyor = new ArrayList<Suitcase>();
+
+    private Lock locket_passenger = new ReentrantLock();        // passengers locket
+    private Condition full = locket_passenger.newCondition();   // if the conveyor is full the passengers have to wait for leaving their suitcase
+
+    private Lock locket_employee = new ReentrantLock();         // employees locket
+    private Condition empty = locket_employee.newCondition();   // if the conveyor is empty the employees have to wait
+
     
-    public void LeaveSuitcase(ArrayList suitcases, ArrayList conveyor){
-        
+    public void LeaveSuitcase(Suitcase suitcases, ArrayList conveyor){
+        try {
+            locket_passenger.lock();
+            while (this.conveyor.lenght()==8){
+                try {
+                    full.await();
+                } catch(InterruptedException ie){ }
+            }
+        }
+        finally{
+            locket_passenger.unlock();
+            empty.signal();
+            //return conveyor.add(suitcase);
+        }
     } 
     
     public void TakeSuitcase (ArrayList conveyor){
-        
+        try{
+            locket_employee.lock();
+            while (this.conveyor.lenght()==0){
+                try {
+                empty.await();
+                } catch(InterruptedException ie){ }
+            }
+        }
+        finally{
+            locket_employee.unlock();
+            full.signal();
+            //return conveyor.remove[0];
+        }
     }
 }
